bc92e6645ca46ea36b052455f1081860ca3280bc
ganeshnatarajan1991 <ganeshnatarajan1991@gmail.com>
2022-01-11 22:53:42 +0530

    taskdone

---
+++ src/org/selenium/test/JavaSample.java
@@ -0,0 +1,11 @@
+package org.selenium.test;
+
+public class JavaSample {
+	private void add() {
+		System.out.println("Hello World");
+	}
+	public static void main(String[] args) {
+		JavaSample j = new JavaSample();
+				j.add();
+	}
+}
